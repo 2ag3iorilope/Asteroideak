358eee36a05675b22507df7bffea82c76e9dd021
ioritzlope45@gmail.com <ioritzlope45@gmail.com>
2024-10-22 09:53:44 +0200

    Last

--- app/src/main/java/com/ikaslea/asteroideak/AcercaDeActivity.java
+++ app/src/main/java/com/ikaslea/asteroideak/AcercaDeActivity.java
@@ -5,7 +5,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AcercaDeActivity extends AppCompatActivity {
-    @Override public void onCreate(Bundle savedInstanceState) {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acercade);
     }
--- app/src/main/java/com/ikaslea/asteroideak/Lokalizazioa.java
+++ app/src/main/java/com/ikaslea/asteroideak/Lokalizazioa.java
@@ -3,25 +3,55 @@ package com.ikaslea.asteroideak;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Lokalizazioa extends AppCompatActivity {
+
+    private Button bAcercaDe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         setContentView(R.layout.main);
 
+
+        bAcercaDe = findViewById(R.id.Button03);
+        Button bIrten = findViewById(R.id.Button04);
+
+        bIrten.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+        if (bAcercaDe != null) {
+            bAcercaDe.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    lanzarAcercaDe(null);
+                }
+            });
+        } else {
+
+            throw new RuntimeException("Button03 ez da aurkitu");
+        }
+
     }
-    public void lanzarAcercaDe(View view){
+
+    public void lanzarAcercaDe(View view) {
         Intent i = new Intent(this, AcercaDeActivity.class);
         startActivity(i);
     }
 
+    public void irten(View view) {
+        finish();
+    }
+
 }
+
+
